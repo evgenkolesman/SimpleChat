bfdde2487aead0f58127b860d3b88ba66084159d
Evgeniy Kolesnikov <glavstroi_e@mail.ru>
2022-11-12 23:30:19 +0300

    files update: update openApi.yaml Readme.md LICENSE openApi.yaml

--- src/main/java/ru/kolesnikov/simplechat/SimpleChatApplication.java
+++ src/main/java/ru/kolesnikov/simplechat/SimpleChatApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SimpleСhatApplication {
+public class SimpleChatApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(SimpleСhatApplication.class, args);
+        SpringApplication.run(SimpleChatApplication.class, args);
     }
 
 }
